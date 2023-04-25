192d63d7ac58462e17f7ab5a66e5afc7d6a6a0ed
Jaine Santos <jainejosiane@gmail.com>
2023-04-25 15:43:45 -0300

    Atualizando produto

---
+++ src/models/Produto.ts
@@ -0,0 +1,12 @@
+import { Categoria } from "./Categoria";
+
+export interface Produto {
+    id: number;
+    nome: string;
+    marca: string;
+    descricao: string;
+    preco: number;
+    qtd_estoque: number;
+    foto: string;
+    categoria: Categoria | null;
+}
